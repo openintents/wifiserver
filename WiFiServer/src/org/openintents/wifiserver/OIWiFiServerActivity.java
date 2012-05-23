decd7115d261b073eedca5118f6b7bd9f3b6b9c2
Stanley F <stanley.foerster@gmail.com>
2012-05-23 23:49:11 +0200

    - added wifiserver project

---
+++ WiFiServer/src/org/openintents/wifiserver/OIWiFiServerActivity.java
@@ -0,0 +1,13 @@
+package org.openintents.wifiserver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OIWiFiServerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
