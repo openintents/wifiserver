956bbf65d2b8e3fa79a422593a66c1973608cca1
Stanley F <stanley.foerster@gmail.com>
2012-05-25 12:00:19 +0200

    - added main layout and corresponding strings

--- WiFiServer/src/org/openintents/wifiserver/OIWiFiServerActivity.java
+++ WiFiServer/src/org/openintents/wifiserver/OIWiFiServerActivity.java
@@ -1,13 +1,10 @@
 package org.openintents.wifiserver;
 
 import android.app.Activity;
-import android.os.Bundle;
 
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.main)
 public class OIWiFiServerActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
 }
