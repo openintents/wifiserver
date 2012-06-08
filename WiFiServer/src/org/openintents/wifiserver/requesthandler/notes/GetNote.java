d6d1e70a97f7439ec98e23001874d325f7c7b17a
Stanley F <stanley.foerster@gmail.com>
2012-06-08 15:15:44 +0200

    - implemented own note cursor to JSON function, because of backward compatibility

--- WiFiServer/src/org/openintents/wifiserver/requesthandler/notes/GetNote.java
+++ WiFiServer/src/org/openintents/wifiserver/requesthandler/notes/GetNote.java
@@ -8,8 +8,8 @@ import org.apache.http.entity.AbstractHttpEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.protocol.HttpContext;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-import org.openintents.wifiserver.util.CursorUtil;
 import org.openintents.wifiserver.util.URLUtil;
 
 import android.content.Context;
@@ -17,7 +17,9 @@ import android.database.Cursor;
 import android.util.Log;
 
 public class GetNote extends NotesHandler {
-    
+
+    private static final String[] PROJECTION = new String[] {"_id", "title", "note", "tags", "created", "modified"};
+
     public GetNote(Context context) {
         super(context);
     }
@@ -28,56 +30,80 @@ public class GetNote extends NotesHandler {
             response.setStatusCode(405);
             return ;
         }
-        
+
         String id = URLUtil.getParameter(request.getRequestLine().getUri(), "id");
-        
+
         if (id == null) {
-            Cursor notesCursor = mContext.getContentResolver().query(mNotesURI, null, null, null, null);
+            Cursor notesCursor = mContext.getContentResolver().query(mNotesURI, PROJECTION, null, null, null);
             if (!notesCursor.moveToFirst()) {
                 response.setStatusCode(404);
                 notesCursor.close();
                 return;
             }
 
-            JSONArray noteList = new JSONArray();
-
-            do {
-                noteList.put(CursorUtil.convertToJSONObject(notesCursor));
-            } while (notesCursor.moveToNext());
-
             try {
-                AbstractHttpEntity entity = new StringEntity(noteList.toString());
+                AbstractHttpEntity entity = new StringEntity(notesToJSONArray(notesCursor).toString());
                 entity.setContentType("application/json");
                 response.setEntity(entity);
             } catch (UnsupportedEncodingException e) {
                 Log.e(TAG, "Failed to create entity!", e);
                 response.setStatusCode(500);
+            } catch (JSONException e) {
+                Log.e(TAG, "Failed to create JSON Array", e);
+                e.printStackTrace();
             }
 
             notesCursor.close();
         } else {
-            Cursor notesCursor = mContext.getContentResolver().query(mNotesURI, null, "_id = ?", new String[] { id }, null);
+            Cursor notesCursor = mContext.getContentResolver().query(mNotesURI, PROJECTION, "_id = ?", new String[] { id }, null);
             if (!notesCursor.moveToFirst()) {
                 response.setStatusCode(404);
                 notesCursor.close();
                 return;
             }
 
-            JSONObject json = CursorUtil.convertToJSONObject(notesCursor);
-
-            if (json == null) {
-                response.setStatusCode(500);
-                return;
-            }
-                
             try {
-                AbstractHttpEntity entity = new StringEntity(json.toString());
+                AbstractHttpEntity entity = new StringEntity(noteToJSONObject(notesCursor).toString());
                 entity.setContentType("application/json");
                 response.setEntity(entity);
             } catch (UnsupportedEncodingException e) {
                 Log.e(TAG, "Failed to create entity!", e);
                 response.setStatusCode(500);
+            } catch (JSONException e) {
+                Log.e(TAG, "Failed to create JSON Object", e);
+                e.printStackTrace();
             }
         }
+    }
+
+    protected JSONObject noteToJSONObject(int id, String title, String note, String tags, long createdDate, long modifiedDate) throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("_id", id);
+        json.put("title", title);
+        json.put("note", note);
+        json.put("tags", tags);
+        json.put("created", createdDate);
+        json.put("modified", modifiedDate);
+
+        return json;
+    }
+
+    protected JSONObject noteToJSONObject(Cursor notesCursor) throws JSONException {
+        return noteToJSONObject(notesCursor.getInt(notesCursor.getColumnIndex("_id")),
+                        notesCursor.getString(notesCursor.getColumnIndex("title")),
+                        notesCursor.getString(notesCursor.getColumnIndex("note")),
+                        notesCursor.getString(notesCursor.getColumnIndex("tags")),
+                        notesCursor.getLong(notesCursor.getColumnIndex("created")),
+                        notesCursor.getLong(notesCursor.getColumnIndex("modified")));
+    }
+
+    protected JSONArray notesToJSONArray(Cursor notesCursor) throws JSONException {
+        JSONArray array = new JSONArray();
+
+        do {
+            array.put(noteToJSONObject(notesCursor));
+        } while (notesCursor.moveToNext());
+
+        return array;
     }
 }
